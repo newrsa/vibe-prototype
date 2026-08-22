aaa90a90c5e9044238def5834b649a3445a7b647
rsapradipsalke <pradip.salke@rightstepahead.com>
2026-08-22 15:47:30 +0530

    Update FTP deployment path to /prototype/vibe-prototype/

--- client-app/src/components/common/ErrorBoundary.tsx
+++ client-app/src/components/common/ErrorBoundary.tsx
@@ -19,7 +19,6 @@ export class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoun
   static getDerivedStateFromError(): ErrorBoundaryState {
     return { hasError: true }
   }
-
   render() {
     if (this.state.hasError) {
       return this.props.fallback ?? <div>Something went wrong.</div>
