7d042c2475fc369424f79fa3a291138b63e0d0e4
rsapradipsalke <pradip.salke@rightstepahead.com>
2026-08-23 07:51:02 +0530

    chnge yml

--- client-app/src/components/common/LoadingSpinner.tsx
+++ client-app/src/components/common/LoadingSpinner.tsx
@@ -2,7 +2,6 @@
 export function LoadingSpinner() {
   return <div>Loading…</div>
 }
-
 export function PageLoader() {
   return <div className="flex h-full items-center justify-center">Loading…</div>
 }
