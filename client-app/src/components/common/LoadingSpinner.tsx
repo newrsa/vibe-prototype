336e0fd02fe4851c4f013475f82a9e25be719e44
rsapradipsalke <pradip.salke@rightstepahead.com>
2026-08-23 21:23:44 +0530

    test 1

--- client-app/src/components/common/LoadingSpinner.tsx
+++ client-app/src/components/common/LoadingSpinner.tsx
@@ -2,6 +2,7 @@
 export function LoadingSpinner() {
   return <div>Loading…</div>
 }
+
 export function PageLoader() {
   return <div className="flex h-full items-center justify-center">Loading…</div>
 }
