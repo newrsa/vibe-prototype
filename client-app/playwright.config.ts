4cc6baf754e437679df7a206cc4217c9960eb367
rsapradipsalke <pradip.salke@rightstepahead.com>
2026-09-11 15:16:34 +0530

    trigger tests

--- client-app/playwright.config.ts
+++ client-app/playwright.config.ts
@@ -69,6 +69,7 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
+
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run dev -- --host 127.0.0.1 --port 5173',
