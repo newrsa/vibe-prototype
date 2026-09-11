c1b45b5c9b8911b7e95d7f38d57d8f49b5cd5cf5
rsapradipsalke <pradip.salke@rightstepahead.com>
2026-09-11 12:18:28 +0530

    force ipv4 for vite

--- client-app/playwright.config.ts
+++ client-app/playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev',
+    command: 'npm run dev -- --host 127.0.0.1 --port 5173',
     url: 'http://127.0.0.1:5173',
     reuseExistingServer: !process.env.CI,
     timeout: 120000,
